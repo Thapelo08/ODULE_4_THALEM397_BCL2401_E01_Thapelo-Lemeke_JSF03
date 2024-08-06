e0b6b0036109cf7b1a55a65e61c903c8824a30a4
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-08-06 11:06:49 +0200

    Updated router configuration: reformatted route definitions, added route names, and updated import statements.

--- my-vue-app/src/router/index.js
+++ my-vue-app/src/router/index.js
@@ -1,18 +1,27 @@
+// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue'; // Path to Home.vue
-import ProductDetail from '../views/ProductDetails.vue';
- // Path to ProductDetail.vue
+import Home from '../views/Home.vue';
+// import About from '../views/ProductDetails.vue';
+import ProductDetails from '../views/ProductDetails.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/product/:id', component: ProductDetail, props: true }
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/product/:id',
+    name: 'ProductDetails',
+    component: ProductDetails,
+    props: true,
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 
 export default router;
-
 
