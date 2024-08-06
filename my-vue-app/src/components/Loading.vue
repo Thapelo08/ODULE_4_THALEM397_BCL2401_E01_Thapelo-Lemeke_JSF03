7ef8a2569bae316e8a308d8bf24f4713920ffd93
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-08-06 10:17:11 +0200

    Refactored Vue components for Navbar, ProductGrid, Home, and ProductDetails views, including changes to templates, scripts, and styles

--- my-vue-app/src/components/Loading.vue
+++ my-vue-app/src/components/Loading.vue
@@ -5,10 +5,18 @@
   </template>
   
   <script>
+  /**
+   * @fileoverview A component that displays a loading indicator.
+   * This component is used to show a loading message to users while data is being fetched or processed.
+   */
+  
   export default {
+    /**
+     * The name of the component.
+     * @type {string}
+     */
     name: 'LoadingComponent',
   };
   </script>
   
- 
   
